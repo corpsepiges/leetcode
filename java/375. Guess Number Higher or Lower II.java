efd68ac1e3ff97535d9c0ec140998a4cc84a5ae8
徐玄清 <524058708@qq.com>
2016-07-18 17:26:45 +0800

    20160718

---
+++ java/375. Guess Number Higher or Lower II.java
@@ -0,0 +1,23 @@
+﻿public class Solution {
+    public int getMoneyAmount(int n) {
+        int[][] matrix=new int[n+1][n+1];
+        return pay(matrix,1,n);
+    }
+    public int pay(int[][] matrix,int s,int e){
+        if(s>=e){
+            return 0;
+        }
+        if(matrix[s][e]!=0){
+            return matrix[s][e];
+        }
+        int ans=Integer.MAX_VALUE;
+        for(int m=s;m<=e;m++){
+            int test=m+Math.max(pay(matrix,s,m-1),pay(matrix,m+1,e));
+            if(test<ans){
+                ans=test;
+            }
+        }
+        matrix[s][e]=ans;
+        return ans;
+    }
+}
