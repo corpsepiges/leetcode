6457bb65d5557c2c3ba376e9d0ac08d53c8ddc2e
徐玄清 <524058708@qq.com>
2016-07-20 17:56:50 +0800

    20160720
    ruby start

---
+++ ruby/001. Two Sum.rb
@@ -0,0 +1,16 @@
+# @param {Integer[]} nums
+# @param {Integer} target
+# @return {Integer[]}
+def two_sum(nums, target)
+	map={}
+	index=0
+	nums.each{|num| map[(target-num)]=index;index+=1}
+	puts map
+	for i in 0..nums.size
+		print i,nums[i],map[nums[i]]
+		if map[nums[i]] && i!=map[nums[i]]
+			return [i,map[nums[i]]]
+		end
+	end
+	return []
+end
