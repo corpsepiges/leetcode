46ea3b9beb98bd1d4468b2e33576c601bdfb0c3c
徐玄清 <524058708@qq.com>
2016-08-09 10:01:51 +0800

    20160809

---
+++ Algorithms/380. Insert Delete GetRandom O(1)/Solution.java
@@ -0,0 +1,43 @@
+﻿import java.util.Random;
+public class RandomizedSet {
+    List<Integer> list;
+    Random r;
+    /** Initialize your data structure here. */
+    public RandomizedSet() {
+        list=new ArrayList<Integer>();
+        r=new Random();
+    }
+    
+    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+    public boolean insert(int val) {
+        Integer v=val;
+        if(list.contains(v)){
+            return false;
+        }
+        list.add(v);
+        return true;
+    }
+    
+    /** Removes a value from the set. Returns true if the set contained the specified element. */
+    public boolean remove(int val) {
+        Integer v=val;
+        if(list.contains(v)){
+            list.remove(v);
+            return true;
+        }
+        return false;
+    }
+    
+    /** Get a random element from the set. */
+    public int getRandom() {
+        return list.get(r.nextInt(list.size()));
+    }
+}
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet obj = new RandomizedSet();
+ * boolean param_1 = obj.insert(val);
+ * boolean param_2 = obj.remove(val);
+ * int param_3 = obj.getRandom();
+ */
