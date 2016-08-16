5ec42ba4529a2e60baf919389c0816b26bf146a1
徐玄清 <524058708@qq.com>
2016-08-16 10:23:14 +0800

    20160816

---
+++ Algorithms/111. Minimum Depth of Binary Tree/Solution.rb
@@ -0,0 +1,22 @@
+﻿# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val)
+#         @val = val
+#         @left, @right = nil, nil
+#     end
+# end
+
+# @param {TreeNode} root
+# @return {Integer}
+def min_depth(root)
+    if root
+        left=root.left ? min_depth(root.left) : nil
+        right=root.right ? min_depth(root.right) : nil
+        left= left ? left : (right ? right : 0)
+        right= right ? right : (left ? left : 0)
+        return 1+[left,right].min()
+    else
+        return 0
+    end
+end
---
+++ Algorithms/112. Path Sum/Solution.rb
@@ -0,0 +1,15 @@
+﻿# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val)
+#         @val = val
+#         @left, @right = nil, nil
+#     end
+# end
+
+# @param {TreeNode} root
+# @param {Integer} sum
+# @return {Boolean}
+def has_path_sum(root, sum)
+    return root ? (!root.left && !root.right ? root.val==sum : has_path_sum(root.left,sum-root.val)||has_path_sum(root.right,sum-root.val)): false
+end
