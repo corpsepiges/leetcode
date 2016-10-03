fd64d6e45446ae40d3496f56158b59bbdc106c7e
徐玄清 <524058708@qq.com>
2016-10-04 01:32:44 +0800

    20161004

---
+++ java/161. One Edit Distance.java
@@ -0,0 +1,22 @@
+﻿public class Solution {
+    public boolean isOneEditDistance(String s, String t) {
+        if (s.length()>t.length()) {
+            return isOneEditDistance(t, s);
+        }
+        if (s.length()+1<t.length()) {
+            return false;
+        }
+        char[] cs=s.toCharArray();
+        char[] ct=t.toCharArray();
+        for (int i = 0; i < cs.length; i++) {
+            if (cs[i]!=ct[i]) {
+                if (s.length()==t.length()) {
+                    return s.substring(i+1,s.length()).equals(t.substring(i+1,t.length()));
+                }else{
+                    return s.substring(i,s.length()).equals(t.substring(i+1,t.length()));
+                }
+            }
+        }
+        return s.length()!=t.length();
+    }
+}
